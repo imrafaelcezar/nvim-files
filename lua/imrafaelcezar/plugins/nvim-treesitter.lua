9f9733592462257c50dcaee596a1f96d6d247abb
imrafaelcezar <imrafaelcezar@gmail.com>
2024-08-16 18:13:24 -0300

    feat: add treesitter and telescope

---
+++ lua/imrafaelcezar/plugins/nvim-treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
